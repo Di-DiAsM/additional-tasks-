e81700fb359b39454be04f0e32375d277e0275a2
Di <dimas2343@mail.ru>
2022-08-08 21:59:21 +0300

    add 001

---
+++ 001 Секретное сообщение при вводе правильного пароля/Program.cs
@@ -0,0 +1,30 @@
+﻿// в переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы дается 3 попытки
+// ввести пароль и увидеть секретное сообщение 
+
+Console.WriteLine("Введите пароль");
+string secretText = "Для Вас секретная информация: 'Сложнее всего начать действовать, все остальное зависит только от упорства!'";
+string Pasword = "123"; // сначала делал без этой переменной, просто if(a == 123), но в условии написано 2 переменных поэтому 
+                   //добавил сюда
+int currentCount = 2;
+
+while(currentCount >= 0)
+{
+    string? text = Console.ReadLine();
+    if(text == Pasword)
+    {
+        Console.WriteLine(secretText);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Неправильный ввод пароля, попробуйте снова, попыток осталось " + currentCount);
+        if(currentCount == 0)
+        {
+           Console.WriteLine("Доступ запрещен");
+           break; 
+        }
+    }
+    currentCount--;
+}
+// примерно так получилось
+
---
+++ 003 Игра угадайка/Program.cs
@@ -0,0 +1,36 @@
+﻿// Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает.
+// Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное 
+// число или меньше. На отгадывание дается три попытки.
+
+int randomNumber = new Random().Next(1,11);
+Console.WriteLine("Угадайте числ от 1 до 10, у Вас три попытки: ");
+int numberOfAttempts = 2;
+
+
+while(true)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if(number == randomNumber)
+    {
+       Console.WriteLine("Верно, вы угадали, это чило: " + randomNumber); 
+       break;
+    }
+    else if(number > randomNumber)
+    {
+        Console.WriteLine("Не угадали, число " + number + " больше загаданного числа. Можете попробовать еще " + numberOfAttempts + " раз.");
+    }
+    else if(number < randomNumber)
+    {
+        Console.WriteLine("Не угадали, число " + number + " меньше загаданного числа. Можете попробовать еще " + numberOfAttempts + " раз.");
+    }
+    if(numberOfAttempts == 0)
+    {
+        Console.WriteLine("Вы не угадали, загаданное число " + randomNumber);
+        break;
+    }
+    numberOfAttempts--;
+}
+
+
+
